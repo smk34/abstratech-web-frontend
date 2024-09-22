cc7ffb6c0ac2226b08181f1240dab9474c818c78
Soumik Sarkar <smksarkar5@gmail.com>
2024-09-22 18:48:52 +0530

    header-part-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,175 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import { Route, Routes } from 'react-router-dom'
+
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            // <RequireAuth>
+              <HomePage />
+            // </RequireAuth>
+          }
+          // loader={<Loader />}
+        />
+        {/* <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <LandingPage />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/roles"
+          element={
+            <RequireAuth>
+              <RoleList />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/permission"
+          element={
+            <RequireAuth>
+              <PermissionForm />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Login />
+            </>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/forgotpassword"
+          element={
+            <>
+              <ForgotPassword />
+            </>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/otp/:email"
+          element={
+            <>
+              <Otp />
+            </>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/projects"
+          element={
+            <RequireAuth>
+              <ProjectList />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/addprojectphase/:id"
+          element={
+            <RequireAuth>
+              <Addprojectphase />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/viewprojectphase/:id"
+          element={
+            <RequireAuth>
+              <Viewprojectphase />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+
+        <Route
+          path="/placeholder"
+          element={
+            <>
+              <PlaceholderLoader />
+            </>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/viewprojectphase/addprojectphase/:pid/:vid"
+          element={
+            <RequireAuth>
+              <EditProjectPhase />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/performances/staff/details/:id"
+          element={
+            <RequireAuth>
+              <UserPerformancesDetails />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+        <Route
+          path="/profile/:id"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+          loader={<Loader />}
+        />
+         {
+        path: "/performances/sales/staff/:id?",
+        element: (
+          <RequireAuth>
+            {" "}
+            <SalesStaffPerformanceDetails />
+          </RequireAuth>
+        ),
+        loader: <Loader />,
+      },
+      <Route path="/performances/sales/staff/:id?"element={
+        <RequireAuth>
+          <SalesStaffPerformanceDetails/>
+        </RequireAuth>
+      }/>
+      <Route path="/resetpassword/:email/:otp"element={
+          <ResetPassword/>
+      }/>
+
+        {routePaths &&
+          routePaths.map((route, index) => <Route key={index} {...route} />)}
+        <Route
+          path="/*"
+          element={
+            <>
+              <PageNotFound />
+            </>
+          }
+          loader={<Loader />}
+        /> */}
+      </Routes>
+    </HashRouter>
   )
 }
 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../layouts/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default HomePage;
