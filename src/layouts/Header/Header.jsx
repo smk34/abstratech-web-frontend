0045431fe605b1ae0c7b95e5b18abd0458165294
Gaurav Dev <subhamkr1995dob@gmail.com>
2024-09-23 23:10:10 +0530

    Logo removed text added

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 // import Logo from "../../assets/logo.jpeg";
 import "./Header.css";
 import { GiHamburgerMenu } from "react-icons/gi";
